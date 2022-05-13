675735356c46a7788996ad1c38195949e1bf8a6d
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-13 14:46:55 -0500

    update

---
+++ Lab3/lab3.c
@@ -0,0 +1,7 @@
+/*
+Write a program that will calculate the revenue of an electronics store for today’s sales.  
+Your program will contain the price for each item and the sales tax – the only thing you need to ask the 
+user is how much of each item was sold. Once your program has done that, you can calculate the 
+revenue for each item (item amount sold * item price) and the total revenue of the store, before and 
+after sales tax is applied. 
+*/
